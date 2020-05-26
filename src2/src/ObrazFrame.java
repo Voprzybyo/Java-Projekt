97fc0243c16de84554c4dc1f7c20be50831441c6
Wojtek <38841320+Voprzybyo@users.noreply.github.com>
2020-05-26 11:43:21 +0200

    Add files via upload

---
+++ src2/src/ObrazFrame.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.io.IOException;
+@SuppressWarnings("serial") // Pozbycie siê warningów...
+
+public class ObrazFrame extends JFrame {
+
+    public ObrazFrame() throws IOException {
+        super("COVID-19");		//Konstruktor klasy nadrzêdnej
+
+        JPanel obrazPanel = new ObrazPanel();
+        add(obrazPanel);		//Dodanie panelu do Ramki
+        setLocation(500,200);	//Po³o¿enie lewego górnego rogu okna
+        pack();					//Metoda pack() ustala rozmiar okna tak, aby mieœci³y siê w nim wszystkie widoczne komponenty.
+        setVisible(true);
+    }
+}
---
+++ src2/src/ObrazPanel.java
@@ -0,0 +1,48 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+@SuppressWarnings("serial") // Pozbycie siê warningów...
+
+public class ObrazPanel extends JPanel {
+	
+    private BufferedImage image;
+
+    static Logger logger = Logger.getLogger(ButtonPanel.class);
+    
+    public ObrazPanel() throws IOException {
+        super();													
+
+        String log4jConfigFile = System.getProperty("user.dir")		//Œcie¿ka do pliku log4j.properties
+                + File.separator + "log4j.properties";
+        PropertyConfigurator.configure(log4jConfigFile);			//Wywo³anie z wy¿ej z³o¿on¹ œcie¿k¹
+        
+        /*adres strony ze zdjêciem do pobrania*/
+        URL url = new URL("https://ocdn.eu/pulscms-transforms/1/l8Ek9kpTURBXy9lMzcxZTU5ZWJlYzg2ZWQ3ZjFlOGRkODQ5ODYzMDUyOS5qcGeTlQMBAM0PoM0IypMFzQMUzQG8kwmmNzFhMDFlBoGhMAE/wizualizacja-koronawirusa.jpg");
+        try {
+            image = ImageIO.read(url); 					//Próba odczytu obrazu z podanego url
+            
+        } catch (MalformedURLException e) {
+        	logger.error("ERROR --> Nie uda³o siê pobraæ i wyœwietliæ obrazu"); //Wy³apanie wyj¹tku w przypadku b³êdu odczytu ze strony
+        }
+        
+        /*Dostosowanie wymiarów ramki do wymiarów obrazka*/
+        Dimension dimension = new Dimension(image.getWidth(), image.getHeight()); 
+        setPreferredSize(dimension);					//Funkcja z JFrame
+    }
+
+    /*Wyœwietlenie obrazu z wykorzystaniem biblioteki Graphics2D*/
+    public void paintComponent(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawImage(image, 0, 0, this); 	// 0,0 --> pozycje lewego górnego rogu obrazu
+    }
+}
+
